b853abc579f5dfaab896cf57c39268a36c109a83
dizpers <dizpers@gmail.com>
2016-12-15 15:15:14 +0700

    Test person adding to addressbook

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -4,7 +4,14 @@ from unittest import TestCase
 class AddressBookTestCase(TestCase):
 
     def test_add_person(self):
-        pass
+        person = Person(
+            'John',
+            'Doe',
+            ['Russian Federation, Kemerovo region, Kemerovo, Kirova street 23, apt. 42'],
+            ['+79834772053']
+        )
+        self.address_book.add_person(person)
+        self.assertIn(person, self.address_book)
 
     def test_add_group(self):
         pass
