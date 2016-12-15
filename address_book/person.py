fb335ed74d9d924816fe6bf712844023abf62e30
dizpers <dizpers@gmail.com>
2016-12-15 15:27:04 +0700

    Add constructor with required arguments to the `Person` class

--- address_book/person.py
+++ address_book/person.py
@@ -2,4 +2,9 @@ __all__ = ['Person']
 
 
 class Person(object):
-    pass
+
+    def __init__(self, first_name, last_name, addresses, phone_numbers):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.addresses = addresses
+        self.phone_numbers = phone_numbers
