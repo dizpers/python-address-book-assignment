7e5240967e926c47301318df4833dd9af1fe9c7c
dizpers <dizpers@gmail.com>
2016-12-15 15:21:21 +0700

    Update person addition test to create address book inside test func + import needed classes from the package

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -1,17 +1,20 @@
 from unittest import TestCase
 
+from address_book import AddressBook, Person
+
 
 class AddressBookTestCase(TestCase):
 
     def test_add_person(self):
+        address_book = AddressBook()
         person = Person(
             'John',
             'Doe',
             ['Russian Federation, Kemerovo region, Kemerovo, Kirova street 23, apt. 42'],
             ['+79834772053']
         )
-        self.address_book.add_person(person)
-        self.assertIn(person, self.address_book)
+        address_book.add_person(person)
+        self.assertIn(person, address_book)
 
     def test_add_group(self):
         pass
