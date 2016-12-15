102a9a667771b5777e610a894533107dd5a0e263
dizpers <dizpers@gmail.com>
2016-12-15 15:06:25 +0700

    Add tests scaffold and the file with requirements

---
+++ tests/test_address_book.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+
+class AddressBookTestCase(TestCase):
+
+    def test_add_person(self):
+        pass
+
+    def test_add_group(self):
+        pass
+
+    def test_find_person_by_first_name(self):
+        pass
+
+    def test_find_person_by_last_name(self):
+        pass
+
+    def test_find_person_by_email(self):
+        passjjj
---
+++ tests/test_person.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class PersonTestCase(TestCase):
+
+    def test_get_groups(self):
+        pass
