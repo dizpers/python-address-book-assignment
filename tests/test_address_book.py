509990ffdaec354b09d34aae63e1851a2e8a2038
dizpers <dizpers@gmail.com>
2016-12-19 15:49:01 +0700

    Add test for finding a person by first name

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -23,7 +23,21 @@ class AddressBookTestCase(TestCase):
         self.assertIn(group, address_book)
 
     def test_find_person_by_first_name(self):
-        pass
+        address_book = AddressBook()
+        john_person = Person(
+            'John',
+            'Doe',
+            ['Russian Federation, Kemerovo region, Kemerovo, Kirova street 23, apt. 42'],
+            ['+79834772053']
+        )
+        ivan_person = Person(
+            'Ivan',
+            'Doe',
+            ['Russian Federation, Kemerovo region, Belovo, Kirova street 42, apt. 13'],
+            ['+79834771122']
+        )
+        found_person = address_book.find(first_name='Ivan')
+        self.assertEqual(found_person, ivan_person)
 
     def test_find_person_by_last_name(self):
         pass
