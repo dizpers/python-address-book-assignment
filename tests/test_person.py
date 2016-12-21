4c7e4e629d154424acf5590a3c65c7e4d30c5aff
dizpers <dizpers@gmail.com>
2016-12-21 15:56:42 +0700

    Test the ability to add some address to the person

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,7 +1,30 @@
 from unittest import TestCase
 
+from address_book import Person
+
 
 class PersonTestCase(TestCase):
 
     def test_get_groups(self):
+        pass
+
+    def test_add_address(self):
+        basic_address = ['Russian Federation, Kemerovo region, Kemerovo, Kirova street 23, apt. 42'],
+        person = Person(
+            'John',
+            'Doe',
+            basic_address,
+            ['+79834772053'],
+            ['john@gmail.com']
+        )
+        person.add_address('new address')
+        self.assertEqual(
+            person.addresses,
+            basic_address + ['new address']
+        )
+
+    def test_add_phone(self):
+        pass
+
+    def test_add_email(self):
         pass
